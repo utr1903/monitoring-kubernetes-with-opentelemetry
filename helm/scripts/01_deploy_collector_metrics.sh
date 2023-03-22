47f65d724427ae7c3a233b43949d78fd4ae21d7f
uturkarslan <ugurturkarslan@gmail.com>
2023-03-22 22:57:20 +0100

    Fix prometheus scraping

--- helm/scripts/01_deploy_collector_metrics.sh
+++ helm/scripts/01_deploy_collector_metrics.sh
@@ -33,3 +33,25 @@ helm upgrade ${otelcollector[name]} \
   --set newrelicOtlpEndpoint=$newrelicOtlpEndpoint \
   --set newrelicLicenseKey=$NEWRELIC_LICENSE_KEY \
   "../charts/metrics"
+
+# # prometheus
+# clusterName="mytestcluster"
+# helm upgrade "prometheus" \
+#   --install \
+#   --wait \
+#   --debug \
+#   --create-namespace \
+#   --namespace "monitoring" \
+#   --set alertmanager.enabled=false \
+#   --set prometheus-pushgateway.enabled=false \
+#   --set kubeStateMetrics.enabled=true \
+#   --set nodeExporter.enabled=true \
+#   --set nodeExporter.tolerations[0].effect="NoSchedule" \
+#   --set nodeExporter.tolerations[0].operator="Exists" \
+#   --set server.remoteWrite[0].url="https://metric-api.eu.newrelic.com/prometheus/v1/write?prometheus_server=${clusterName}" \
+#   --set server.remoteWrite[0].bearer_token=$NEWRELIC_LICENSE_KEY \
+#   "prometheus-community/prometheus"
+
+# # Flags for agent mode
+# --set server.defaultFlagsOverride[0]="--enable-feature=agent" \
+# --set server.defaultFlagsOverride[1]="--storage.agent.retention.max-time=30m" \
