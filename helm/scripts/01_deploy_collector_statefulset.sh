f03cb668df7a714d4d0d2744813ae2cae9610e5c
uturkarslan <ugurturkarslan@gmail.com>
2023-03-24 17:14:16 +0100

    Remove mode

--- helm/scripts/01_deploy_collector_statefulset.sh
+++ helm/scripts/01_deploy_collector_statefulset.sh
@@ -27,7 +27,6 @@ helm upgrade ${otelcollector[name]} \
   --create-namespace \
   --namespace ${otelcollector[namespace]} \
   --set name=${otelcollector[name]} \
-  --set mode=${otelcollector[mode]} \
   --set ports.prometheus.port=${otelcollector[prometheusPort]} \
   --set newrelic.opsteam.endpoint=$newrelicOtlpEndpoint \
   --set newrelic.opsteam.licenseKey=$NEWRELIC_LICENSE_KEY \
