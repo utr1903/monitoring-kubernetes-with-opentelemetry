f69ee51e7d5b9e34f0fbaf4b67a1a67ca51bf08a
uturkarslan <ugurturkarslan@gmail.com>
2023-04-18 16:57:58 +0200

    Comment in adding & updating helm repos

--- helm/scripts/01_deploy_collectors.sh
+++ helm/scripts/01_deploy_collectors.sh
@@ -31,9 +31,9 @@ otelcollectors["statefulsetPrometheusPort"]=8888
 ###################
 
 # Repositories
-# helm repo add bitnami https://charts.bitnami.com/bitnami
-# helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-# helm repo update
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
 
 # nodeexporter
 helm upgrade ${nodeexporter[name]} \
