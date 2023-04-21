237bf7e4e6bb655fe70be313aae3beecf790ab91
uturkarslan <ugurturkarslan@gmail.com>
2023-04-21 15:59:53 +0200

    Fix widget title

--- monitoring/terraform/04_dashboard_kube_apiserver.tf
+++ monitoring/terraform/04_dashboard_kube_apiserver.tf
@@ -34,9 +34,9 @@ resource "newrelic_one_dashboard" "kube_apiserver" {
       }
     }
 
-    # Node Capacities
+    # Pod statuses per nodes
     widget_line {
-      title  = "Node Capacities"
+      title  = "Pod statuses per nodes"
       row    = 1
       column = 9
       height = 3
